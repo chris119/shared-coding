237333f6cf6cb43b11f78df5e9ff51df03885af1
chris119 <chrisahern2@gmail.com>
2016-07-29 10:37:51 -0700

    Update error.py

--- error.py
+++ error.py
@@ -2,15 +2,20 @@
 # print the days of all 12 months
 def daysInMonth(month):
 	days = 30
-	if (month < 7):
-		if (month / 2 == 1):
+	if (month <= 7):
+#needs a less than or equals to because this part is to separate the double 31 months		
+		if (month % 2 == 1):
+#needs a percent sign so that it involves odds and evens			
 			days = 31
-		if (month = 2):
+		if (month == 2):
+#needed another equals sign so that it means if month is 2			
 			days = 28
 	else:
-		if (month / 2 == 0):
+		if (month % 2 == 0):
+#same percent fix as above			
 			days = 31
 	return days
 
-for i in range(12):
+for i in range(1, 13):
+#adjusted range	
 	print daysInMonth(i)
